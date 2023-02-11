78494267b885e577b261e0cdf5910339953b71a9
consvic <cocoy_shuffle@me.com>
2023-02-10 23:46:49 -0600

    tweaked wallet validation

--- src/components/Deposit/Deposit.tsx
+++ src/components/Deposit/Deposit.tsx
@@ -5,7 +5,7 @@ import { depositFunds } from '~/api/transaction'
 
 export function Deposit(): JSX.Element {
   const queryClient = useQueryClient()
-  const { isConnected } = useAccount()
+  const { isConnected: isConnectedEth } = useAccount()
   const [amount, setAmount] = useState<number>()
 
   const mutation = useMutation(depositFunds, {
@@ -21,6 +21,14 @@ export function Deposit(): JSX.Element {
     setAmount(numVal)
   }
 
+  function onClickDeposit(): void {
+    if (!amount || !isConnectedEth) return
+    mutation.mutate({
+      transactionType: 'deposit',
+      amount,
+      chainName: isConnectedEth ? 'ethereum-mainnet' : 'solana-mainnet',
+    })
+  }
   return (
     <form>
       <label htmlFor="deposit" className="text-sm text-slate-600 pr-2">
@@ -30,14 +38,15 @@ export function Deposit(): JSX.Element {
         name="deposit"
         className="bg-slate-200 mr-2 border-b border-slate-900 px-4 py-3"
         type="number"
-        disabled={!isConnected}
+        disabled={!isConnectedEth}
         value={amount}
         onChange={({ target }) => onChangeAmount(target.value)}
         min={0}
       />
       <button
         type="button"
-        disabled={!isConnected}
+        disabled={!isConnectedEth || !amount || mutation.isLoading}
+        onClick={onClickDeposit}
         className="text-sm text-slate-600 rounded-lg bg-slate-200 disabled:bg-slate-400 disabled:text-slate-500 px-4 py-3 shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)]"
       >
         Deposit
--- src/utils/general.ts
+++ src/utils/general.ts
@@ -3,6 +3,9 @@ import camelCase from 'lodash/camelCase'
 import mapKeys from 'lodash/mapKeys'
 import { Transaction } from '~/api/types'
 
+// TODO add a generic json type here
+type GenericJson = any
+
 export function calculateTotalUsd(transactions: Transaction[]): number {
   const totalWithDecimals = transactions.reduce((prev, curr) => {
     if (curr.transactionType === 'deposit') {
@@ -22,10 +25,19 @@ export function removeExtraDecimals(value: number, toFixed = 2): number {
   return Number(parsed.toFixed(toFixed))
 }
 
-export function fromSnakeToCamelCase(arr: any[]): any[] {
-  return arr.map((t: any) => mapKeys(t, (v, k) => camelCase(k)))
+export function fromSnakeToCamelCase(arr: GenericJson[]): GenericJson[] {
+  return arr.map((t: GenericJson) => mapKeys(t, (v, k) => camelCase(k)))
 }
 
-export function fromCamelToSnakeCase(arr: any[]): any[] {
-  return arr.map((t: any) => mapKeys(t, (v, k) => snakeCase(k)))
+export function fromCamelToSnakeCase(arr: GenericJson[]): GenericJson[] {
+  return arr.map((t: GenericJson) => mapKeys(t, (v, k) => snakeCase(k)))
+}
+
+export const truncateAddress = (
+  address: string,
+  numberOfCharacters = 5
+): string => {
+  return `${address.slice(0, numberOfCharacters)}...${address.slice(
+    -numberOfCharacters
+  )}`
 }
