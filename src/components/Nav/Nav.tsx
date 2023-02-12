3df39d3440b8ee9769d4125f4f6c0972004c9abf
consvic <cocoy_shuffle@me.com>
2023-02-12 15:06:07 -0600

    added changes to keep eth and & sol consistent

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -41,7 +41,9 @@ export function Nav(): JSX.Element {
           <CustomRainbowButton disabled={!currentChain} />
         )}
         {(currentChain === 'Solana' || isConnectedSol) && (
-          <SolanaButton className="text-sm text-slate-600 rounded-lg bg-slate-200 disabled:bg-slate-400 disabled:text-slate-500 px-4 py-3 shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)]" />
+          <SolanaButton className="text-sm text-slate-600 rounded-lg bg-slate-200 disabled:bg-slate-400 disabled:text-slate-500 px-4 py-3 shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)]">
+            {!isConnectedSol ? 'Connect' : null}
+          </SolanaButton>
         )}
       </div>
       {showWallets && (
--- src/components/Nav/styles.ts
+++ src/components/Nav/styles.ts
@@ -5,6 +5,8 @@ export const SolanaButton = styled(WalletMultiButton)`
   color: rgb(71, 85, 105);
   font-weight: 400;
   font-size: 14px;
+  border-radius: 0.5rem;
+
   :not([disabled]):hover {
     background-color: rgb(226, 232, 240);
   }
